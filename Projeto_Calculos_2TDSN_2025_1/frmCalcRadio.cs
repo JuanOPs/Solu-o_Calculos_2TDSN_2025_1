ac81f4f04229ee816771d3b1d8b66a157564d7ba
JuanOPs <juanoliviopalmeiras@hotamil.com>
2025-04-03 22:11:59 -0300

    03/04/2025

--- Projeto_Calculos_2TDSN_2025_1/frmCalcRadio.cs
+++ Projeto_Calculos_2TDSN_2025_1/frmCalcRadio.cs
@@ -25,8 +25,31 @@ namespace Projeto_Calculos_2TDSN_2025_1
             a = decimal.Parse(textBox1.Text);
             b = Convert.ToDecimal(textBox2.Text);
 
-            potencia = Math.Pow((double)a, (double)b);
-            lblResultado.Text = potencia.ToString();
+            if (rdbSomar.Checked)
+            {
+                decimal add = a + b;
+                lblResultado.Text = add.ToString();
+            }
+            else if (rdbSubtrair.Checked)
+            {
+                decimal sub = a - b;
+                lblResultado.Text = sub.ToString();
+            }
+            else if(rdbMultiplicar.Checked)
+            {
+                decimal mul = a * b;
+                lblResultado.Text = mul.ToString();
+            }
+            else if (rdbDividir.Checked)
+            {
+                decimal div = a / b;
+                lblResultado.Text = div.ToString();
+            }
+            else if (rdbPotenciacao.Checked)
+            {
+                potencia = Math.Pow((double)a, (double)b);
+                lblResultado.Text += potencia.ToString();
+            }
             
         }
 
@@ -44,6 +67,7 @@ namespace Projeto_Calculos_2TDSN_2025_1
         {
             textBox1.Clear();
             textBox2.Clear();
+            lblResultado.Text = "";
         }
     }
 }
